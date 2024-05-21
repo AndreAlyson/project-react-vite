9585829f0569aa78c6cc2519b4a8c91a956fd7fb
André Alyson <andrealyson1@hotmail.com>
2024-05-21 16:56:59 -0300

    Iserido cores a navBar

---
+++ project-react-vite/src/components/NavBar.tsx
@@ -0,0 +1,21 @@
+
+function NavBar() {
+  return (
+    <>
+    <div className='navBar'>
+      <p>PRODUTOS DE MODA COM ATÉ 70% OFF</p>
+      <p>Acessibilidade</p>
+      <p>Tire suas dúvidas</p>
+    </div>
+    <div className='carrinho'>
+      <h3>Netshoes</h3>
+      <input type="text" placeholder='O que você está procurando?'/>
+      <p>🤍 Lista de desejos</p>
+      <p>Entrar</p>
+      🛒
+    </div>
+    </>
+  )
+}
+
+export default NavBar;
